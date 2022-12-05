8efa348a941a968b223b62e526c8470fe3bcb4fe
redoneElli <redwane.bouselham@editions-ellipses.fr>
2022-12-05 17:34:36 +0100

    init : création structure

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,35 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { id, name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/portrait/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    function getUserDescDOM() {
+        const description = document.createElement('p');
+        const localisation = document.createElement('span');
+        localisation.textContent = city + ', ' + country;
+        const devise = document.createElement('span');
+        devise.textContent = tagline;
+        const tarif = document.createElement('span');
+        tarif.textContent = price + '€/jours';
+        description.appendChild(localisation);
+        description.appendChild(devise);
+        description.appendChild(tarif);
+        return (description);
+    }
+
+
+    return { name, picture, getUserCardDOM, getUserDescDOM }
 }
+
+export { photographerFactory };
